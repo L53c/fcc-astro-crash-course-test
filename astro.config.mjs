784eb6c134b27c164bcaa1876cdb1556cc0502b2
L53c <aurelio1978@gmail.com>
2023-10-19 05:14:12 +0100

    ssr enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import nodejs from '@astrojs:node'
 
 // https://astro.build/config
 export default defineConfig({
